e70dc5365cf7dc89c7747c096d6eb3581ae0a3d6
sonalgill <sonalgill37@gmail.com>
2022-09-05 20:12:46 +0530

    done

--- src/controllers/getDetails.js
+++ src/controllers/getDetails.js
@@ -1,20 +1,30 @@
-const { get } = require('mongoose')
+const mongoose = require('mongoose')
 const blogModel = require('../models/blogModel')
-
-const getBlogs = async function(req, res){
-    try{
+const getBlogs = async function (req, res) {
+    try {
+        let obj = req.query
         let authorId = req.query.authorId
         let category = req.query.category
         let tags = req.query.tags
+        let subcategory = req.query.subcategory
+        let isValid = mongoose.Types.ObjectId.isValid(authorId)
 
-        let getBlogs = await blogModel.find({ isPublished: true}, {isDeleted: false})
-        if(!getBlogs) { return res.status(404).send({status: false, data: "No such blog found"})}
-     //   let filtered = await getBlogs.find({authorId: authorId}, {category: category}, {tags:tags})
-        res.status(200).send({status : true , data : getBlogs})
-
-    }
-    catch(error){
-        res.status(500).send({ status: false, message: error.message})
+    
+        if(Object.keys(obj).length != 0){
+            if(authorId){
+                if(!isValid){return res.status(400).send({status: false, message :"Not a valid Author ID"})}
+            }
+            let filtered = await blogModel.find({authorId : authorId})
+           return res.status(200).send({status: true, message : filtered})
+        }
+        else{
+            let getBlogs = await blogModel.find({ $and: [{ isPublished: true }, { isDeleted: false }] })
+            if (!getBlogs.length) { return res.status(404).send({ status: false, data: "No such blog found" }) }
+             return res.status(200).send({ status: true, data: getBlogs }) 
+        }
+}
+    catch (error) {
+        res.status(500).send({ status: false, message: error.message })
     }
 }
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,24 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const authorController = require("../controllers/authorController.js")
-<<<<<<< HEAD
 const getBlog = require("../controllers/getDetails")
 
-=======
 const blogController =   require("../controllers/blogController")
->>>>>>> 411d20da77339ced17a069214cbd71f2f997a924
 
 
 // create author api
 router.post("/authors", authorController.createAuthor)
-<<<<<<< HEAD
 
 router.get("/getBlogs", getBlog.getBlogs)
 
-=======
 // create blog api
 router.post("/blogs",blogController.createBlog)
->>>>>>> 411d20da77339ced17a069214cbd71f2f997a924
+
 
 
 
