7fe5fa074df4fce7f4c9b57b4bc31b9265377949
sonalgill <sonalgill37@gmail.com>
2022-09-06 00:19:27 +0530

    done

--- src/controllers/getDetails.js
+++ src/controllers/getDetails.js
@@ -3,33 +3,20 @@ const blogModel = require('../models/blogModel')
 const getBlogs = async function (req, res) {
     try {
         let obj = req.query
-        let authorId = req.query.authorId
-        let category = req.query.category
-        let tags = req.query.tags
-        let subcategory = req.query.subcategory
+        let { authorId, category, tags, subcategory} = obj
         let isValid = mongoose.Types.ObjectId.isValid(authorId)
 
         if (Object.keys(obj).length != 0) {
             if (authorId) {
                 if (!isValid) { return res.status(400).send({ status: false, message: "Not a valid Author ID" }) }
             }
-            let filtered = await blogModel.find(
-                {
-                    $and:
-                        [
-                            { $or: [{undefined : authorId}, {authorId : authorId}] },
-                            { $or: [{undefined : category}, {category : category}] },
-                            { $or: [{undefined : tags}, {tags : {$in: [tags]}}] },
-                            { $or: [{undefined : subcategory}, {subcategory : subcategory}] },
-                            { $and: [{undefined : authorId}, {authorId : authorId}] },
-                            { $and: [{undefined : category}, {category : category}] },
-                            { $and: [{undefined : tags}, {tags : {$in: [tags]}}] },
-                            { $and: [{undefined : subcategory}, {subcategory : subcategory}] }
-                        ]
-                })
-                console.log(authorId)
-                console.log(category)
-                //if(filtered.length == 0){ return res.status(400).send({ status: false, message: "No such data found"})}
+            let filter = {}
+            if(authorId != null){ filter.authorId = authorId}
+            if(category != null){ filter.category = category}
+            if(tags != null){ filter.tags = {$in:[tags]}}
+            if(subcategory != null){ filter.subcategory = {$in:[subcategory]}}
+            let filtered = await blogModel.find(filter)
+            if(filtered.length == 0){ return res.status(400).send({ status: false, message: "No such data found"})}
             return res.status(200).send({ status: true, message: filtered })
         }
         else {
