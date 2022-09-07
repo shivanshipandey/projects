8e56e13f89dea0398e76e62b459b57cfd18881d8
sonalgill <sonalgill37@gmail.com>
2022-09-07 18:17:14 +0530

    done

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -7,20 +7,20 @@ const createAuthor = async function (req, res) {
      try {
           let authorData = req.body
           let { fname, lname, email } = req.body
-          let chek1 = /^[a-zA-Z ]+$/.test(fname)
-          let chek2 = /^[a-zA-Z ]+$/.test(lname)
-          if (chek1 == false || chek2 == false) {
+          let check1 = /^[a-zA-Z ]+$/.test(fname)
+          let check2 = /^[a-zA-Z ]+$/.test(lname)
+          if (check1 == false || check2 == false) {
                return res.status(400).send({ status: false, message: "Please enter letters only, don't enter special character or digit" })
           }
           if (!validator.isEmail(email)) {
-               return res.status(400).send({ status: false, msg: "please enter valid e-mail id" })
+               return res.status(400).send({ status: false, message: "please enter valid e-mail id" })
           }
           let checkemail = await authorModel.findOne({ email: email })
           if (checkemail) {
-               return res.status(409).send({ status: false, msg: "this e-mail id is already registered" })
+               return res.status(409).send({ status: false, message: "this e-mail id is already registered" })
           }
           let authorStored = await authorModel.create(authorData)
-          res.status(201).send({ status: true, msg: authorStored })
+          res.status(201).send({ status: true, message: authorStored })
      }
      catch (error) {
           res.status(500).send({ status: false, message: error.message })
