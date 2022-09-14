bafe86edc6e95b9ff17f50ba611e875895400673
Aakash Ghosh <aakash.duke.2019@gmail.com>
2022-09-14 12:33:36 +0530

    4th push

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -3,7 +3,6 @@ const collegeModel = require("../models/collegeModel");
 const internModel = require("../models/internModel");
 
 const createColleges = async function (req, res) {
-<<<<<<< HEAD
   try {
     const logoRegex =
       /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
@@ -15,48 +14,6 @@ const createColleges = async function (req, res) {
       return res
         .status(400)
         .send({ status: false, message: "please enter Data" });
-=======
-    try {
-
-        let data = req.body
-        let { name, fullName, logoLink } = data
-        
-        let dataBody = Object.keys(data)
-        if (dataBody.length == 0) {
-            return res.status(400).send({ status: false, msg: 'please enter Data' })
-        }
-        if (!name) {
-            return res.status(400).send({ status: false, msg: 'name is required' })
-        }
-        let nameAlreadyExist = await collegeModel.findOne({ name: name })
-        if (nameAlreadyExist) {
-            return res.status(400).send({ status: false, msg: 'name already exist' })
-        }
-        if (!fullName) {
-            return res.status(400).send({ status: false, msg: 'fullName is required' })
-        }
-        if (!logoLink) {
-            return res.status(400).send({ status: false, msg: 'logoLink is required' })
-        }
-
-        //Body should not exceed the length by 4
-
-        if(dataBody.length>4){
-            res.status(400).send({status: false, mssg : "Only name, fullName, logoLink, and idDeleted are allowed in the request body"})
-        }
-
-        //Space will not be tolerated
-
-        if(name.includes(" ")){
-            res.status(400).send({status : false, msg : "Space is not allowed"})
-        }
-        
-        let collegeData = await collegeModel.create(data)
-        return res.status(201).send({ status: true, msg: 'college data created successfully', data: collegeData })
-
-    } catch (error) {
-        return res.status(500).send({ status: false, Error: error.message })
->>>>>>> fef1790c0628fd8ce60fcd04f5e192ccd1fb7552
     }
     if (!name) {
       return res
