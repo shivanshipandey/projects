da3da10f1037c785d1b3bee74f511a51b66c4c26
Aakash Ghosh <aakash.duke.2019@gmail.com>
2022-09-14 15:13:41 +0530

    ag

--- src/route/route.js
+++ src/route/route.js
@@ -15,4 +15,12 @@ router.post('/functionup/interns', createInterns)
 
 router.get('/functionup/collegeDetails', getInternsFromColleges )
 
+router.all("/*", function (req, res) {
+    res.status(404).send({
+        status: false,
+        message: "Make Sure Your Endpoint is Correct !!!"
+    })
+})
+
+
 module.exports= router
