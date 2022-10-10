891150eb2453f376db056b934beecf4d2cf856c2
RUPALI <109238130+RUPALI1999KUMARI@users.noreply.github.com>
2022-10-10 16:54:00 +0530

    first push

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express =require('express');
+const mongoose=require('mongoose');
+const route = require('./routes/route');
+const app=express();
+
+app.use(express.json());
+
+let url="mongodb+srv://project5productsManagementGroup-42:myGnQEOp010y0N42@cluster0.rf4tgvq.mongodb.net/group-42";
+
+let port=process.env.PORT||3000;
+
+mongoose.connect(url, {useNewUrlParser:true})
+.then(()=>"MongoDB is connected")
+.catch(err=>console.log(err))
+
+
+app.use('/', route);
+
+app.listen(port, ()=>{
+    console.log("express app running on port "+port);
+})
