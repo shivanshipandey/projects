db6befd76b17e4be871307131378150b70a34393
RUPALI <109238130+RUPALI1999KUMARI@users.noreply.github.com>
2022-10-17 13:36:44 +0530

    delete api done

--- src/controllers/cartControllers.js
+++ src/controllers/cartControllers.js
@@ -23,5 +23,28 @@ const getCartData=async function(req,res){
     }
 }
 
+const deleteCart = async (req, res) => { 
+    try {
+        let paramsId = req.params.userId
+        if (!isValidObjectId(userId)) {
+            return res.status(400).send({status: false, msg: "Invalid userId"});
+             }
+     let findUser=await userModel.findById({_id:userId})
+        if(!findUser)return res.status(404).send({status:false,message:"User not found"})
+        // Authentication
+        let tokenId=req.userId
+        // console.log(tokenID)
+         if (tokenId !=userId)return res.status(401).send({status:false,message:"Unauthorise Access"})
+        let items = [];
+        let totalPrice = 0
+        let totalItems = 0
+        const cartGet = await cartModel.findOneAndUpdate({ userId: paramsId }, { items: items, totalPrice: totalPrice, totalItems: totalItems }, { new: true });
+        return res.status(204).send({ status: true, message: 'Success', data: cartGet });
+    }
+    catch (err) {
+        res.status(500).send(err.message)
+    }
+}
 
-module.exports={getCartData}
+
+module.exports={getCartData,deleteCart}
